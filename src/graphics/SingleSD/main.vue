57421d5242e15ad3b2749aa7b32bc58b6c3eb5b1
yagamuu <ygmpeer@gmail.com>
2024-02-18 04:07:50 +0900

    Fix Single SD clipPath

--- src/graphics/SingleSD/main.vue
+++ src/graphics/SingleSD/main.vue
@@ -7,17 +7,15 @@ import BaseOverlay from '../components/views/BaseOverlay.vue';
 import InfoBox from '../components/InfoBox.vue';
 import TimerAndEstimate from '../components/TimerAndEstimate.vue';
 
-/*
 const gamePosition = (): Box[] => {
-  const x = 415;
-  const y = 32;
-  const width = 1470;
-  const height = 826;
+  const x = 582;
+  const y = 27;
+  const width = 1104;
+  const height = 830;
   return [[x, x + width, y, y + height]];
 };
 
 const clipPath = computed(() => calculateClipPath(gamePosition()));
-*/
 
 
 // Set the title of this page.
@@ -25,8 +23,7 @@ useHead({ title: 'SingleSD' });
 </script>
 
 <template>
-  <BaseOverlay>
-  <!-- <BaseOverlay :clip-path="clipPath">  -->
+  <BaseOverlay :clip-path="clipPath">
     <div class="cloud"/>
     <div class="logo"/>
     <div class="video_box"></div>
