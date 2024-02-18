8ce92701cd7f2cb76c30eaa126f86b7050016876
den <moto1047@gmail.com>
2024-02-19 01:18:10 +0900

    Fix Setup CSS

--- src/graphics/components/SetupSchedule/OndeckSchedule.vue
+++ src/graphics/components/SetupSchedule/OndeckSchedule.vue
@@ -36,11 +36,13 @@ const minutes = computed(() => (Math.floor(props.upcomingStartInSeconds / 60) % 
         <span>{{ minutes }}</span>分後
       </p>
     </div>
-    <span class="title">
-      {{ runData?.game }}
-    </span>
-    <span class="runner">
-      {{ runData?.category }} / Runner : {{ runners }}
-    </span>
+    <div class="next_title">
+      <span class="title">
+        {{ runData?.game }}
+      </span>
+      <span class="runner">
+        {{ runData?.category }} / Runner : {{ runners }}
+      </span>
+    </div>
   </div>
 </template>
--- src/graphics/components/SetupSchedule/UpnextSchedule.vue
+++ src/graphics/components/SetupSchedule/UpnextSchedule.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable max-len -->
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import type { RunData } from '@mysrtafes2024-layouts/types/schemas/speedcontrol';
 import { useSetupInformation } from '@mysrtafes2024-layouts/composable';
 
@@ -27,16 +27,53 @@ setInterval(() => {
   displaySetupInformation.value = (displaySetupInformation.value + 1) % length;
 }, 20000);
 
+/**
+ * タイトル文字改行切り替え対応
+ */
+const titleClass = ref('');
+let observer: ResizeObserver | null = null;
+// クラス変更フラグ
+let isChangingClass = false;
+
+// props.runData の変化を監視
+watch(() => props.runData, (newVal, oldVal) => {
+  const titleElement = document.querySelector('.title');
+  titleClass.value = '';
+
+  if (titleElement && newVal !== oldVal) {
+    if (observer) observer.disconnect(); // 前回の監視を停止
+
+    // 新たに監視を開始
+    observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.target === titleElement) {
+          if (isChangingClass) {
+            isChangingClass = false;
+          } else if (entry.contentRect.height > 180) {
+            isChangingClass = true;
+            titleClass.value = '-isLong';
+          } else {
+            titleClass.value = '';
+          }
+        }
+      });
+    });
+
+    observer.observe(titleElement);
+  }
+});
 </script>
 
 <template>
   <div class="setup_box_1">
-    <span class="title">
-      {{ runData?.game }}
-    </span>
-    <span class="runner">
-      {{ runData?.category }} / Runner : {{ runners }}
-    </span>
+    <div class="title_box">
+      <span class="title" :class="titleClass">
+        {{ runData?.game }}
+      </span>
+        <span class="runner">
+        {{ runData?.category }} / Runner : {{ runners }}
+      </span>
+    </div>
     <span class="detail">
       <span
         v-for="(information, index) in setupInformationArray?.data"
